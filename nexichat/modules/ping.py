fd069eb7211887b8da054b3ef8adc5a2f3d1dcd7
bsona0023 <bsona0423@gmail.com>
2024-07-25 09:47:57 +0530

    Update ping.py

--- nexichat/modules/ping.py
+++ nexichat/modules/ping.py
@@ -20,20 +20,20 @@ from nexichat.modules.helpers import PNG_BTN
 
 # Random Start Images
 IMG = [
-    "https://graph.org/file/210751796ff48991b86a3.jpg",
-    "https://graph.org/file/7b4924be4179f70abcf33.jpg",
-    "https://graph.org/file/f6d8e64246bddc26b4f66.jpg",
-    "https://graph.org/file/63d3ec1ca2c965d6ef210.jpg",
-    "https://graph.org/file/9f12dc2a668d40875deb5.jpg",
-    "https://graph.org/file/0f89cd8d55fd9bb5130e1.jpg",
-    "https://graph.org/file/e5eb7673737ada9679b47.jpg",
-    "https://graph.org/file/2e4dfe1fa5185c7ff1bfd.jpg",
-    "https://graph.org/file/36af423228372b8899f20.jpg",
-    "https://graph.org/file/c698fa9b221772c2a4f3a.jpg",
-    "https://graph.org/file/61b08f41855afd9bed0ab.jpg",
-    "https://graph.org/file/744b1a83aac76cb3779eb.jpg",
-    "https://graph.org/file/814cd9a25dd78480d0ce1.jpg",
-    "https://graph.org/file/e8b472bcfa6680f6c6a5d.jpg",
+    "https://telegra.ph/file/1d8ac0a1a98ec5ced9124.jpg",
+    "https://telegra.ph/file/36213460b3aa6069d763f.jpg",
+    "https://telegra.ph/file/416d87dc64c6508736dca.jpg",
+    "https://telegra.ph/file/d6701b75d7228128926d1.jpg",
+    "https://telegra.ph/file/1d8ac0a1a98ec5ced9124.jpg",
+    "https://telegra.ph/file/36213460b3aa6069d763f.jpg",
+    "https://telegra.ph/file/416d87dc64c6508736dca.jpg",
+    "https://telegra.ph/file/d6701b75d7228128926d1.jpg",
+    "https://telegra.ph/file/1d8ac0a1a98ec5ced9124.jpg",
+    "https://telegra.ph/file/36213460b3aa6069d763f.jpg",
+    "https://telegra.ph/file/416d87dc64c6508736dca.jpg",
+    "https://telegra.ph/file/d6701b75d7228128926d1.jpg",
+    "https://telegra.ph/file/1d8ac0a1a98ec5ced9124.jpg",
+    "https://telegra.ph/file/d6701b75d7228128926d1.jpg",
 ]
 
 
@@ -62,7 +62,7 @@ async def ping(_, message: Message):
 
     ms = (datetime.now() - start).microseconds / 1000
     await loda.edit_text(
-        text=f"нey вαву!!\n{nexichat.name} ᴄʜᴀᴛʙᴏᴛ ιѕ alιve 🥀 αnd worĸιng ғιne wιтн a pιng oғ\n➥ `{ms}` ms\n\n<b>|| мαdє ωιтн ❣️ ву [ᴠɪᴘ ʙᴏʏ](https://t.me/{OWNER_USERNAME}) ||</b>",
+        text=f"нey вαву!!\n{nexichat.name} ᴄʜᴀᴛʙᴏᴛ ιѕ alιve 🥀 αnd worĸιng ғιne wιтн a pιng oғ\n➥ `{ms}` ms\n\n<b>|| мαdє ωιтн ❣️ ву [𝙈𝙍 𝘿𝙀𝙑𝙄𝙇](https://t.me/{OWNER_USERNAME}) ||</b>",
         reply_markup=InlineKeyboardMarkup(PNG_BTN),
     )
     if message.chat.type == ChatType.PRIVATE:
