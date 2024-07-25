b18ac600441b99234161166528b009c80ca81567
bsona0023 <bsona0423@gmail.com>
2024-07-25 10:02:20 +0530

    Update read.py

--- nexichat/modules/helpers/read.py
+++ nexichat/modules/helpers/read.py
@@ -2,7 +2,7 @@ from config import OWNER_USERNAME, SUPPORT_GRP
 from nexichat import nexichat
 
 START = f"""
-**๏ ʜᴇʏ,\n\n ɪ ᴀᴍ 𝐀𝐚𝐥𝐢𝐲𝐚 💞\n🥀ʜᴏᴡ ᴀʀᴇ ʏᴏᴜ...??
+**๏ ʜᴇʏ,\n\n ɪ ᴀᴍ 𝚂𝙸𝚉𝚄𝙺𝙰 💞\n🥀ʜᴏᴡ ᴀʀᴇ ʏᴏᴜ...??
 """
 
 HELP_READ = f"""
@@ -15,8 +15,6 @@ HELP_READ = f"""
 
 TOOLS_DATA_READ = f"""
 <u>**ᴛᴏᴏʟs ғᴏʀ {nexichat.name} ᴀʀᴇ:**</u>
-**➻ ᴜsᴇ /repo ғᴏʀ ɢᴇᴛᴛɪɴɢ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ!**
-**──────────────**
 **➻ ᴜsᴇ /ping ғᴏʀ ᴄʜᴇᴄᴋɪɴɢ ᴛʜᴇ ᴘɪɴɢ ᴏғ {nexichat.name}**
 **──────────────**
 **➻ ᴜsᴇ /id ᴛᴏ ɢᴇᴛ ʏᴏᴜʀ ᴜsᴇʀ ɪᴅ, ᴄʜᴀᴛ ɪᴅ ᴀɴᴅ ᴍᴇssᴀɢᴇ ɪᴅ ᴀʟʟ ɪɴ ᴀ sɪɴɢʟᴇ ᴍᴇssᴀɢᴇ.**
@@ -32,7 +30,7 @@ CHATBOT_READ = f"""
 <b>||©️ @{OWNER_USERNAME}||</b>
 """
 
-SOURCE_READ = f"**ʜᴇʏ, ᴛʜᴇ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ ᴏғ [{nexichat.name}](https://t.me/{nexichat.username}) ɪs ɢɪᴠᴇɴ ʙᴇʟᴏᴡ.**\n**✨ ᴊᴏɪɴ ᴏᴜʀ ᴄʜᴀɴɴᴇʟ ✯**\n**──────────────────**\n**ʜᴇʀᴇ ɪs ᴛʜᴇ [𝐋𝐎𝐕𝐄 𝐒𝐓𝐀𝐓𝐔𝐒❤️‍🩹](https://t.me/LOVE_ME_BABBY)**\n**──────────────────**\n**ɪғ ʏᴏᴜ ғᴀᴄᴇ ᴀɴʏ ᴘʀᴏʙʟᴇᴍ ᴛʜᴇɴ ᴄᴏɴᴛᴀᴄᴛ ᴀᴛ [sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ](https://t.me/{SUPPORT_GRP}).\n<b>||©️ @{OWNER_USERNAME}||</b>"
+SOURCE_READ = f"**ʜᴇʏ, ᴛʜᴇ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ ᴏғ [{nexichat.name}](https://t.me/{nexichat.username}) ɪs ɢɪᴠᴇɴ ʙᴇʟᴏᴡ.**\n**✨ ᴊᴏɪɴ ᴏᴜʀ ᴄʜᴀɴɴᴇʟ ✯**\n**──────────────────**\n**ʜᴇʀᴇ ɪs ᴛʜᴇ [𝙳𝙴𝚅𝙸𝙻 𝙱𝙾𝚃'𝚂](https://t.me/devilbots971)**\n**──────────────────**\n**ɪғ ʏᴏᴜ ғᴀᴄᴇ ᴀɴʏ ᴘʀᴏʙʟᴇᴍ ᴛʜᴇɴ ᴄᴏɴᴛᴀᴄᴛ ᴀᴛ [sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ](https://t.me/{SUPPORT_GRP}).\n<b>||©️ @{OWNER_USERNAME}||</b>"
 
 ADMIN_READ = f"sᴏᴏɴ"
 
@@ -40,7 +38,7 @@ ABOUT_READ = f"""
 **➻ [{nexichat.name}](https://t.me/{nexichat.username}) ɪs ᴀɴ ᴀɪ ʙᴀsᴇᴅ ᴄʜᴀᴛ-ʙᴏᴛ.**
 **➻ [{nexichat.name}](https://t.me/{nexichat.username}) ʀᴇᴘʟɪᴇs ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ᴛᴏ ᴀ ᴜsᴇʀ.**
 **➻ ʜᴇʟᴘs ʏᴏᴜ ɪɴ ᴀᴄᴛɪᴠᴀᴛɪɴɢ ʏᴏᴜʀ ɢʀᴏᴜᴘs.**
-**➻ ᴊᴏɪɴ [❤️ ꜰʀɪᴇɴᴅꜱʜɪᴘ ɢʀᴏᴜᴘ ❤️](https://t.me/FRIENDSHIP_CHAT_GROUP0) ᴡɪᴛʜ [ʙᴅɢ ᴡɪɴ ᴏꜰꜰɪᴄɪᴀʟ](https://t.me/BDGWINGAME3) **
+**➻ ᴊᴏɪɴ [𝙳𝙴𝚅𝙸𝙻 𝙱𝙾𝚃 𝚂𝚄𝙿𝙿𝙾𝚁𝚃](https://t.me/devilbotsupport) ᴡɪᴛʜ [𝙰𝙺𝙰𝚃𝚂𝚄𝙺𝙸](https://t.me/akatsuki976) **
 **──────────────**
 **➻ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ ʙᴜᴛᴛᴏɴs ɢɪᴠᴇɴ ʙᴇʟᴏᴡ ғᴏʀ ɢᴇᴛᴛɪɴɢ ʙᴀsɪᴄ ʜᴇʟᴩ ᴀɴᴅ ɪɴғᴏ ᴀʙᴏᴜᴛ [{nexichat.name}](https://t.me/{nexichat.username})**
 """
