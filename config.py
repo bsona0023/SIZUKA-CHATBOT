2bfd62670b884b60049d06d29d5ab12fd84906ba
bsona0023 <bsona0423@gmail.com>
2024-07-25 10:04:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "6435225"
+API_ID = "21436816"
 # -------------------------------------------------------------
-API_HASH = "4e984ea35f854762dcde906dce426c2d"
+API_HASH = "c269918dddddbc041d536207cab72155"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = getenv("OWNER_ID", "1808943146")
-SUPPORT_GRP = "TG_FRIENDSS"
-UPDATE_CHNL = "VIP_CREATORS"
-OWNER_USERNAME = "THE_VIP_BOY"
+OWNER_ID = getenv("OWNER_ID", "6107980637")
+SUPPORT_GRP = "devilbotsupport"
+UPDATE_CHNL = "devilbots971"
+OWNER_USERNAME = "mrdevil12"
 
